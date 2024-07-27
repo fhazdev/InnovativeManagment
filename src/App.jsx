1cf8268fa128315bade55af7c7cffd896520590d
Francisco Herrera <fjhshadow@gmail.com>
2024-07-26 17:52:18 -0700

    Add cloudflare analytics script

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
+import { Helmet } from 'react-helmet';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import Home from './components/Home';
@@ -15,20 +16,25 @@ function App() {
   });
 
   return (
-    <Router>
-      <div className="App">
-        <Header />
-        
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/services" element={<Services />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
+    <>
+      <Helmet>
+        <script defer src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon='{"token": "be7908e1691441009ceea93dd63a0d39"}'></script>
+      </Helmet>
+      <Router>
+        <div className="App">
+          <Header />
 
-        <Footer />
-      </div>
-    </Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/services" element={<Services />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+
+          <Footer />
+        </div>
+      </Router>
+    </>
   )
 }
 
