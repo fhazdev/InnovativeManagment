59058ef1e60b8fb856aad64a860c411b040f8eea
Francisco Herrera <fjhshadow@gmail.com>
2024-08-10 21:35:02 -0700

    Add pics and text to services. Add background to home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,6 @@
-{/* <img className="img-responsive" src="./src/images/trend-6551546_640.png" /> */ }
-import Button from 'react-bootstrap/Button';
 import { useNavigate } from 'react-router-dom';
+import { Container, Row, Col, Button } from 'react-bootstrap';
+import '/src/assets/styles.css';
 
 const Home = () => {
     const navigate = useNavigate();
@@ -14,26 +14,41 @@ const Home = () => {
     };
 
     return (
-        <div>
-            <header className="bg-dark py-5">
-                <div className="container px-5">
-                    <div className="row gx-5 justify-content-center">
-                        <div className="col-lg-6">
-                            <div className="text-center my-5">
-                                <h1 className="display-5 fw-bolder text-white mb-2">Begin your journey to Financial Freedom</h1>
-                                <p className="lead text-white-50 mb-4">Your trusted partner for small business bookkeeping solutions.</p>
-                                <div className="d-grid gap-3 d-sm-flex justify-content-sm-center">
-                                    {/* <a className="btn btn-primary btn-lg px-4 me-sm-3" href="#features">Get Started</a>
-                                    <a className="btn btn-outline-light btn-lg px-4" href="#!">Learn More</a> */}
-                                    <Button className="px-4 me-sm-3" variant="primary" size="lg" onClick={handleGetStarted}>Get Started</Button>
-                                    <Button className="px-4" variant="secondary" size="lg" onClick={handleLearnMore}>Learn More</Button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </header>
-        </div>
+        <>
+        <Container fluid className="bg-dark py-5 home-background d-flex align-items-center" style={{ minHeight: '100vh' }}>
+            <Row className="justify-content-center w-100">
+                <Col lg={6}>
+                    <Container className="text-center my-5">
+                        {/* <h2 className="lead text-white-50 mb-5">
+                            Your trusted partner for small business bookkeeping solutions.
+                        </h2> */}
+                        <Row className="justify-content-center home-button-row">
+                            <Col xs="auto">
+                                <Button
+                                    variant="primary"
+                                    size="lg"
+                                    className="px-4 me-sm-3"
+                                    onClick={handleGetStarted}
+                                >
+                                    Get Started
+                                </Button>
+                            </Col>
+                            <Col xs="auto">
+                                <Button
+                                    variant="secondary"
+                                    size="lg"
+                                    className="px-4"
+                                    onClick={handleLearnMore}
+                                >
+                                    Learn More
+                                </Button>
+                            </Col>
+                        </Row>
+                    </Container>
+                </Col>
+            </Row>
+        </Container>
+        </>
     );
 }
 
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -2,6 +2,7 @@ import Card from 'react-bootstrap/Card';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image';
 import '/src/assets/styles.css';
 
 const Services = () => {
@@ -10,7 +11,7 @@ const Services = () => {
             <header className="bg-dark py-5">
                 <h1 className="text-center text-white">Services</h1>
             </header>
-            <Container className="mt-5">
+            {/* <Container className="mt-5">
                 <Row>
                     <Col>
                     <Card className="px-3 py-3 services-card" border="dark">
@@ -35,6 +36,68 @@ const Services = () => {
                             <Card.Text>This is a calculated business activity that allows business owners to plan and track organic growth in their revenue. It lets businesses assign their limited funds toward a centered effort to acclimate to the fluctuations in the industry driven by digital disruption and differentiate from competitors.</Card.Text>
                         </Card.Body>
                     </Card>
+                    </Col>
+                </Row>
+            </Container> */}
+            <Container className="pt-2">
+                <Row className="pb-5 align-items-center">
+                    <Col className="d-flex justify-content-center">
+                        <Image className="img-fluid mx-auto d-block" src="/images/budgetingPlanningMeeting.png" />
+                    </Col>
+                    <Col className="d-flex align-self-center">
+                        <Card className="w-100 home-card">
+                            <Card.Body>
+                                <Card.Title className="mb-5 text-center"><h1>Budgeting/Planning</h1></Card.Title>
+                                <Card.Text>
+                                    <p>I can assist in setting up annual and five-year budgets/plans tailored to your business needs. My detailed reporting and analysis of results keep you informed and proactive in meeting your financial goals.</p>
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+                <Row className="mt-5 pb-5 align-items-center">
+                    <Col className="d-flex align-self-center">
+                        <Card className="w-100 home-card">
+                            <Card.Body>
+                                <Card.Title className="mb-5 text-center"><h1>Financial Management</h1></Card.Title>
+                                <Card.Text>
+                                    <p>I can align your operations to meet financial targets, providing you with easy-to-read dashboards and reports to monitor your progress. My goal is to ensure your business runs smoothly and efficiently, keeping you on track to achieve your financial objectives.</p>
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col className="d-flex justify-content-center">
+                        <Image className="img-fluid mx-auto d-block" src="/images/financialMgmt.png" />
+                    </Col>
+                </Row>
+                <Row className="mt-5 align-items-center">
+                    <Col className="d-flex justify-content-center">
+                        <Image className="img-fluid mx-auto d-block" src="/images/bookkeepingServices.png" />
+                    </Col>
+                    <Col className="d-flex align-self-center">
+                        <Card className="w-100 home-card">
+                            <Card.Body>
+                                <Card.Title className="mb-5 text-center"><h1>Bookkeeping</h1></Card.Title>
+                                <Card.Text>
+                                    <p>I can help you set up and manage an accounting system, whether it’s QuickBooks or another platform, ensuring accurate records of revenue, expenses, and assets. I also offer cash flow reporting to keep you informed of your financial position.</p>
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+                <Row className="mt-5 pb-5 align-items-center">
+                    <Col className="d-flex align-self-center">
+                        <Card className="w-100 home-card">
+                            <Card.Body>
+                                <Card.Title className="mb-5 text-center"><h1>Financial Analysis</h1></Card.Title>
+                                <Card.Text>
+                                    <p>I provide comprehensive financial analysis to support your decision-making process. Whether it’s make-or-buy decisions, return on investment evaluations, cost-benefit analyses, or tax planning, we can set up any type of report or schedule to meet your needs.</p>
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col className="d-flex justify-content-center">
+                        <Image className="img-fluid mx-auto d-block" src="/images/financialAnalysis.png" />
                     </Col>
                 </Row>
             </Container>
